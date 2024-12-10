08a0c13e115336fb4443a7c015f7cccae4746f82
Liam Eckert <liameckert17@gmail.com>
2024-12-10 07:39:58 -0500

    rotated the paddle for traditional pong. Delete brick class since it is no longer needed

--- engine/__init__.py
+++ engine/__init__.py
@@ -26,36 +26,48 @@ def ball_intersect_bounds(ball: Ball, width: int, height: int) -> None:
         ball.vel[1] = -ball.vel[1]
         ball.acc[1] = 0
         
-def paddle_intersect_bounds(paddle: Paddle, width: int) -> None:
-    if paddle.pos[0] < 0:
-        # collide with left boundary
-        paddle.pos[0] = 0
-        paddle.vel[0] = -paddle.vel[0]
-        paddle.acc[0] = 0
-    elif paddle.pos[0] + paddle.width > width:
-        # collide with right boundary
-        paddle.pos[0] = width - paddle.width
-        paddle.vel[0] = -paddle.vel[0]
-        paddle.acc[0] = 0
+def paddle_intersect_bounds(paddle: Paddle, height: int) -> None:
+    if paddle.pos[1] < 0:
+        # collide with top boundary
+        paddle.pos[1] = 0
+        paddle.vel[1] = -paddle.vel[1]
+        paddle.acc[1] = 0
+    elif paddle.pos[1] + paddle.height > height:
+        # collide with bottom boundary
+        paddle.pos[1] = height - paddle.height
+        paddle.vel[1] = -paddle.vel[1]
+        paddle.acc[1] = 0
         
 def ball_intersect_paddle(ball: Ball, paddle: Paddle) -> None:
     # a buffer to prevent visual overlap
-    buffer = 5
+    buffer = 2
     
-    # calculate the bottom of the ball and the top of the paddle
-    ball_bottom = np.subtract(ball.pos, np.array([0, ball.radius]))
-    paddle_top = np.add(paddle.pos[1], paddle.height)
+    # calculate the ball left and right bounds
+    ball_left = ball.pos[0] - ball.radius
+    ball_right = ball.pos[0] + ball.radius
     
-    # check if the ball is within the paddle's x bounds
-    x_overlap = (ball.pos[0] + ball.radius > paddle.pos[0]) and (ball.pos[0] - ball.radius < paddle.pos[0] + paddle.width)
+    # calc the ball top and bottom bounds
+    ball_top = ball.pos[1] - ball.radius
+    ball_bottom = ball.pos[1] + ball.radius
     
-    # check if the ball is within the paddle's y bounds
-    y_overlap = ball_bottom[1] < (paddle_top + buffer)
+    # calc the paddles y range
+    paddle_top = paddle.pos[1] - buffer
+    paddle_bottom = paddle.pos[1] + paddle.height + buffer
     
-    # if the ball is within the paddle's x and y bounds, bounce off the paddle
-    if x_overlap and y_overlap:
-        ball.vel[1] = -ball.vel[1]
-        ball.acc[1] = 0
+    # calc the paddle left and right bounds
+    paddle_left = paddle.pos[0] - buffer
+    paddle_right = paddle.pos[0] + paddle.width + buffer
+    
+    # ball is within the paddle's y range
+    in_y_range = ball_top < paddle_bottom and ball_bottom > paddle_top
+    
+    # ball is within the paddle's x range
+    in_x_range = ball_left < paddle_right and ball_right > paddle_left
+    
+    # ball is within the paddle's y range and x range
+    if in_y_range and in_x_range:
+        ball.vel = np.negative(ball.vel)
+        ball.acc = np.negative(ball.acc)
 
 
 # Define common colors
@@ -75,27 +87,31 @@ class Engine:
         paddle (Paddle): Player paddle object
     """
 
-    def __init__(self, width: int, height: int):
+    def __init__(self, screen_width: int, screen_height: int):
         """Initialize game engine with screen dimensions.
 
         Args:
             width (int): Screen width
             height (int): Screen height
         """
-        self.width = width
-        self.height = height
+        self.screen_width = screen_width
+        self.screen_height = screen_height
         self.ball = Ball(
             radius=10,
-            x=width/2,
-            y=height/2,
-            vel=(0, 400),
-            color=BLUE
+            x=self.screen_width/2,
+            y=self.screen_height/2,
+            vel=(100, 2),
+            color=(255, 105, 180)
         )
-        self.paddle = Paddle(
-            x=width/2,
-            y=(height - 200) / 4,
-            width=100,
-            height=10,
+        paddle_width = self.screen_width / 25
+        paddle_height = self.screen_height / 4
+        paddle_x = paddle_width / 2
+        paddle_y = self.screen_height / 2 - paddle_height / 2
+        self.player_paddle = Paddle(
+            x=paddle_x,
+            y=paddle_y,
+            width=paddle_width,
+            height=paddle_height,
             color=WHITE
         )
 
@@ -106,10 +122,10 @@ class Engine:
             dt (float): Time step delta
         """
         self.ball.update(dt)
-        self.paddle.update(dt)
-        ball_intersect_bounds(self.ball, self.width, self.height)
-        paddle_intersect_bounds(self.paddle, self.width)
-        ball_intersect_paddle(self.ball, self.paddle)
+        self.player_paddle.update(dt)
+        ball_intersect_bounds(self.ball, self.screen_width, self.screen_height)
+        paddle_intersect_bounds(self.player_paddle, self.screen_width)
+        ball_intersect_paddle(self.ball, self.player_paddle)
         
     def is_game_over(self):
         """Check if game is over.
@@ -120,7 +136,7 @@ class Engine:
             bool: True if game is over, False otherwise
         """
         # TODO: refine this
-        return self.ball.pos[1] > self.paddle.pos[1]
+        return self.ball.pos[1] > self.player_paddle.pos[1]
 
 
 
@@ -136,7 +152,7 @@ if __name__ == "__main__":
     def on_draw():
         window.clear()
         ball_shape = engine.ball.to_pyglet()
-        paddle_shape = engine.paddle.to_pyglet()
+        paddle_shape = engine.player_paddle.to_pyglet()
         ball_shape.draw()
         paddle_shape.draw()
     
